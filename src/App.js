9efff59a2da084b61d9478109dc3fcbbb6495da9
rahidaliyev <rahidaliyev18@gmail.com>
2022-05-03 01:02:41 +0400

    show hide button uncompleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-// import logo from './logo.svg';
 import './App.css';
 // import Title from './components/Title/Title';
 // import Footer from './components/Footer/Footer';
 // import Subscribe from './components/Main/Main';
 // import Liked from './components/Extrus/Extrus'
 import UserLogin from './components/lastMain/NewMain';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/lastMain/NewMain.js
+++ src/components/lastMain/NewMain.js
@@ -1,8 +1,17 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 import "./NewMain.css";
 
-class UserLogin extends Component {
-  render() {
+class UserLogin extends Component{
+  constructor(){
+    super();
+    this.state={
+      showPassword: false
+    }
+  }
+  render(){
+console.warn(this.state.showPassword);
+  
+
     return (
       <div className="container">
         <h1>Sign In</h1>
@@ -16,10 +25,19 @@ class UserLogin extends Component {
           <br />
       
           <br />
+          <div className="col-3">
+<div className="input-group mb-3 shadow border rounded">
           <input 
           type="password" 
           placeholder="Password"
           name="fname" />
+          <button
+          onClick={()=>this.state({ showPassword: !this.state.showPassword})}
+           >Show
+           </button>
+</div>
+          </div>
+          
           <br />
           <button className="btnClass" type="button">
           <p>Log In
